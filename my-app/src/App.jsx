4cefa04dadc21e3e44bd4ef4a3e35f17f621cf98
Luna Bujalesky <lunitabruja@gmail.com>
2025-09-18 02:51:07 -0300

    logo

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,33 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+{/*import reactLogo from './assets/wintekalogoborrador.png'*/}
+import viteLogo from './assets/wintekalogoborrador.png'
 import './App.css'
+import NavBar from "./components/Navbar";
+import ItemListContainer from "./components/Itemlistcontainer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
+     {/* logo */}
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+      </div>
+      <h1>Winteka Showroom</h1>
+      <nav>
+      <ul>
+        <li><Link to="/">Inicio</Link></li>
+        <li><Link to="/productos">Productos</Link></li>
+        <li><Link to="/talles">Talles</Link></li>
+        <li><Link to="/contacto">Contacto</Link></li>
+      </ul>
+    </nav>
+     {/* carrito de compra*/}
+    <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        </a> 
     </>
   )
 }

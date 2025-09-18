757988e85edd1f30e64bc0df0615ea161ee3515e
Luna Bujalesky <lunitabruja@gmail.com>
2025-09-18 18:12:24 -0300

    desbugeado

--- src/components/Itemlistcontainer.jsx
+++ src/components/Itemlistcontainer.jsx
@@ -1,20 +1,9 @@
-import ItemListContainer from './components/ItemListContainer';
-
 function ItemListContainer({ greeting }) {
   return (
     <div>
       {greeting}
-      {/* lo puse como elementos de texto para poder conformar la frase mas estéticamente integrada con el diseño */}
     </div>
-    
   );
 }
-{/* si no como la consigna: */}
-function ItemListContainer(props) {
-    return (
-      <h2>{props.greeting}</h2>
-    );
-  }
-  
 
 export default ItemListContainer;

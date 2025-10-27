f585fc4ddd3ed54cd265f307c35e9ae880bbc780
Luna Bujalesky <lunitabruja@gmail.com>
2025-10-27 17:58:56 -0300

    mejor reorganizacion en app.jsx y componente index

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import { useState, useRef  } from "react";
 import { Route, Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
-import Productos from "./data/Productos";
+
 
 /*Componentes*/
 import "./App.css";
@@ -10,7 +10,6 @@ import "./index.css";
 import NavBar from "./components/Navbar";
 import ItemListContainer from "./components/ItemListContainer";
 import CarrouselLanding from "./components/CarrouselLanding";
-import CardItem from "./components/CardItem";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -27,51 +26,30 @@ function App() {
   const scrollToProducts = () => {
     productsRef.current?.scrollIntoView({ behavior: "smooth" });
   };
+
   return (
-    <>
     <BrowserRouter basename="/WintekaShowroom">
-       <header>
-       <Header /> 
+      <header>
+        <Header />
         <NavBar scrollToProducts={scrollToProducts} />
       </header>
-     
       
+      <main>
         <Routes>
-        <Route path="/" element={
-            <>
-              <CarrouselLanding
-                greeting={
-                  <>
-                    <h3 style={{ fontFamily: "QuencyDemo" }}>BIENVENIDO</h3>
-                    <p style={{ fontFamily: "QuencyDemo" }}>a tu tienda</p>
-                    <h1 style={{ fontFamily: "QuencyDemo" }}>Whismiscal</h1>
-                    <h4 style={{ fontFamily: "QuencyDemo" }}>Gothic de confianza</h4>
-                    <p style={{ fontFamily: "QuencyDemo" }}> Hoy 20% off con transferencia</p>
-                  </>
-                }
-              />
-              <div ref={productsRef}>
-              <ItemListContainer style={{ display: "flex", gap: "20px" }} />
-              </div>
-            </>
-          } />
-         
+          <Route path="/" element={<Inicio ref={productsRef} />} />
           <Route path="/categoria/:category" element={<ItemListContainer />} />
           <Route path="/talles" element={<Talles />} />
           <Route path="/contacto" element={<Contacto />} />
           <Route path="/ProductDetail/:id" element={<ProductDetail />} />
-          { /* <Route path="/productos/:id" element={<ProductDetailPage />} />*/}
           <Route path="*" element={<NotFound />} />
         </Routes>
-        <main>
-        
-        </main>
-        <footer>
-          <Footer />
-        </footer>
-      </BrowserRouter>
-   
-    </>
+      </main>
+
+
+      <footer>
+        <Footer />
+      </footer>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,26 @@
-function Index() {
-    return (
-           
-        <body>
-        <div id="root"></div>
-      </body>
-        
-    );
-  }
-  
-  export default  Index;
+import CarrouselLanding from '../components/CarrouselLanding';
+import ItemListContainer from '../components/ItemListContainer';
+import { forwardRef } from 'react';
+
+const Inicio = forwardRef((props, ref) => {
+  return (
+    <>
+      <CarrouselLanding
+        greeting={
+          <>
+            <h3 style={{ fontFamily: "QuencyDemo" }}>BIENVENIDO</h3>
+            <p style={{ fontFamily: "QuencyDemo" }}>a tu tienda</p>
+            <h1 style={{ fontFamily: "QuencyDemo" }}>Whismiscal</h1>
+            <h4 style={{ fontFamily: "QuencyDemo" }}>Gothic de confianza</h4>
+            <p style={{ fontFamily: "QuencyDemo" }}> Hoy 20% off con transferencia</p>
+          </>
+        }
+      />
+      <div ref={ref}>
+        <ItemListContainer style={{ display: "flex", gap: "20px" }} />
+      </div>
+    </>
+  );
+});
+
+export default Inicio;
